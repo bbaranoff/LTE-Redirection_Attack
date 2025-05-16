18a857f96fb3bbea9461522ee14e544df9024334
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-16 23:52:38 +0200

    fix

--- osmo_egprs/scripts/tun.sh
+++ osmo_egprs/scripts/tun.sh
@@ -3,8 +3,8 @@
 #
 #!/bin/sh
 ip l del apn0
-echo nameserver 1.1.1.1 > tee /etc/resolv.conf
-echo nameserver 1.0.0.1 > tee /etc/resolv.conf
+echo nameserver 1.1.1.1 > /etc/resolv.conf
+echo nameserver 1.0.0.1 >> /etc/resolv.conf
 ip tuntap add dev apn0 mode tun
 ip addr add 176.16.32.0/20 dev apn0
 ip addr add 2001:780:44:2100::/56 dev apn0

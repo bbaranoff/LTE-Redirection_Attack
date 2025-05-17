9cf2e3d7f35dc67dd833461fbd1071fe48e5506b
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-17 17:42:01 +0200

    commit

--- reset_tables.sh
+++
@@ -1,8 +0,0 @@
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -F
-sudo iptables -X
-sudo iptables -t nat -F
-sudo iptables -t nat -X
-sudo iptables -t mangle -F
-sudo iptables -t mangle -X

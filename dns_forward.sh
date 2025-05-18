617e80df0bd4a7ecbcf848fc3175fc7caac43f8d
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-18 14:02:34 +0200

    commit

--- dns_forward.sh
+++ dns_forward.sh
@@ -2,4 +2,6 @@ sudo bash reset_tables.sh
 #sudo iptables -t nat -I PREROUTING -i apn0 -p udp --dport 53 -j DNAT --to-dest 1.1.1.1
 sudo iptables -t nat -A POSTROUTING -s 176.16.32.0/20 -o enp114s0 -j MASQUERADE
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -w net.ipv6.conf.all.forwarding=1
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1

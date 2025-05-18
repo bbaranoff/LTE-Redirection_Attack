9cc40f51a3fdcf686ed36fa1f16fd5dd0a01d3dc
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-18 14:09:47 +0200

    commit

--- scripts/myenv/bin/activate.fish
+++ scripts/myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/nirvana/LTE-Redirection_Attack/scripts/myenv
+set -gx VIRTUAL_ENV /home/nirvana/LTE-Redirection_attack/scripts/myenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH

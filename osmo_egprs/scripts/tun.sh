801fab3eada60c3750d5fb1f4dd9f1277b64f5f2
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-20 18:35:42 +0200

    commit

--- osmo_egprs/scripts/tun.sh
+++ osmo_egprs/scripts/tun.sh
@@ -3,7 +3,7 @@
 #
 #!/bin/sh
 ip l del apn0
-echo nameserver 192.168.1.254 >> /etc/resolv.conf
+echo nameserver 192.168.1.254 > /etc/resolv.conf
 ip tuntap add dev apn0 mode tun
 ip addr add 176.16.32.0/20 dev apn0
 #ip addr add 2001:780:44:2100::/56 dev apn0

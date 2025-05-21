75e7f92d278f1658592a2b1b9222e47a40187a63
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-21 08:11:33 +0200

    Adding asterisk support

--- osmo_egprs/scripts/tun.sh
+++ osmo_egprs/scripts/tun.sh
@@ -3,8 +3,8 @@
 #
 #!/bin/sh
 ip l del apn0
-echo nameserver 212.27.40.240 > /etc/resolv.conf
-echo nameserver 212.27.40.241 >> /etc/resolv.conf
+echo nameserver 192.168.1.254 > /etc/resolv.conf
+echo nameserver 212.27.40.240 >> /etc/resolv.conf
 ip tuntap add dev apn0 mode tun
 ip addr add 176.16.32.0/20 dev apn0
 #ip addr add 2001:780:44:2100::/56 dev apn0

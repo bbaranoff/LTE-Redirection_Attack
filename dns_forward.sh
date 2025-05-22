5574390ff1c46741358f56e70175f5d0a7a85fdd
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-23 00:19:58 +0200

    commit

--- dns_forward.sh
+++ dns_forward.sh
@@ -7,7 +7,5 @@ iptables -t nat -F
 iptables -t nat -X
 iptables -t mangle -F
 iptables -t mangle -X
-iptables -A POSTROUTING -o  enp114s0 -s 176.16.32.0/28 ! -d 176.16.32.0/28 -t nat -j MASQUERADE
-ip6tables -A POSTROUTING -o  enp114s0 -s 2001:780:44:2100:0:0:0:0/56 ! -d 2001:780:44:2100:0:0:0:0/56 -t nat -j MASQUERADE
+iptables -A POSTROUTING -o  enp114s0 -s 172.17.0.2/16 ! -d 172.17.0.2/16 -t nat -j MASQUERADE
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -w net.ipv6.conf.all.forwarding=1
--- osmo_egprs/scripts/tun.sh
+++ osmo_egprs/scripts/tun.sh
@@ -3,13 +3,9 @@
 #
 #!/bin/sh
 ip l del apn0
-
-# Put gateway IP in /etc/resolv.conf
-echo $(ip r | grep ^def | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+') > route
-route=$(sed 's/ .*//g' route)
-
-echo nameserver $route > /etc/resolv.conf
+echo nameserver 1.1.1.1 > /etc/resolv.conf
+echo nameserver 1.0.0.1 >> /etc/resolv.conf
 ip tuntap add dev apn0 mode tun
-ip addr add 176.16.32.0/28 dev apn0
-ip addr add 2001:780:44:2100:0:0:0:0/56 dev apn0
+ip addr add 176.16.32.0/20 dev apn0
+ip addr add 2001:780:44:2100::/56 dev apn0
 ip link set apn0 up

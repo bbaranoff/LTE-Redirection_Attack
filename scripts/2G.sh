b7481f39f483274e2376f457e9e74d4f9ee43180
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-23 01:23:17 +0200

    commit

--- scripts/2G.sh
+++ scripts/2G.sh
@@ -1,4 +1,4 @@
 #
 # Copyright 2025  by Bastien Baranoff
 #
-sudo docker run -it --privileged -v /dev:/dev --cap-add=all --device /dev/bus/usb:/dev/bus/usb --device /dev/net/tun:/dev/net/tun --cgroupns=host --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup osmo_egprs-app ./run.sh
+sudo docker run --net host -it --privileged -v /dev:/dev --cap-add=all --device /dev/bus/usb:/dev/bus/usb --device /dev/net/tun:/dev/net/tun --cgroupns=host --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup osmo_egprs-app ./run.sh

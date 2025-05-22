bc69cd75586d9f4bdc52c98943ba3e13a26b59c1
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-22 17:50:19 +0200

    commit

--- dns_forward.sh
+++ dns_forward.sh
@@ -8,6 +8,5 @@ iptables -t nat -X
 iptables -t mangle -F
 iptables -t mangle -X
 iptables -A POSTROUTING -o  enp114s0 -s 176.16.32.0/28 ! -d 176.16.32.0/28 -t nat -j MASQUERADE
-
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo sysctl -w net.ipv6.conf.all.forwarding=1

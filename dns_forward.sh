b9ed5c0e4d6ac42cdf1a5c89d5b9c2d13f32bfdb
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-23 16:15:49 +0200

    commit

--- dns_forward.sh
+++ dns_forward.sh
@@ -8,10 +8,11 @@ iptables -t nat -X
 iptables -t mangle -F
 iptables -t mangle -X
 iptables -A POSTROUTING -o enp114s0 -t nat -s 176.16.32.0/20 ! -d 176.32.16.0/20 -j MASQUERADE
+iptables -t nat -I PREROUTING -i apn0 -p udp --dport 53 -j DNAT --to-dest 8.8.8.8
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 

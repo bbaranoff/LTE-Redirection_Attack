33d8e650d194c23f000aeb10960fe5fcda6b32cb
bbaranoff <bastienbaranoff@hotmail.fr>
2025-05-23 16:04:47 +0200

    keep cool

--- dns_forward.sh
+++ dns_forward.sh
@@ -7,23 +7,11 @@ iptables -t nat -F
 iptables -t nat -X
 iptables -t mangle -F
 iptables -t mangle -X
-iptables -A POSTROUTING -o  enp114s0 -s 176.16.32.0/28 ! -d 176.16.32.0/28 -t nat -j MASQUERADE
-ip6tables -A POSTROUTING -o  enp114s0 -s 2001:780:44:2100:0:0:0:0/56 ! -d 2001:780:44:2100:0:0:0:0/56 -t nat -j MASQUERADE
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -w net.ipv6.conf.all.forwarding=1
+iptables -A POSTROUTING -o enp114s0 -t nat -s 176.16.32.0/20 ! -d 176.32.16.0/20 -j MASQUERADE
+echo 1 > /proc/sys/net/ipv4/ip_forward
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
--- osmo_egprs/scripts/tun.sh
+++ osmo_egprs/scripts/tun.sh
@@ -3,9 +3,7 @@
 #
 #!/bin/sh
 ip l del apn0
-echo nameserver 1.1.1.1 > /etc/resolv.conf
-echo nameserver 1.0.0.1 >> /etc/resolv.conf
+echo nameserver 192.168.1.254 > /etc/resolv.conf
 ip tuntap add dev apn0 mode tun
 ip addr add 176.16.32.0/20 dev apn0
-ip addr add 2001:780:44:2100::/56 dev apn0
 ip link set apn0 up
